d50f6b49284f3188203aef6dba6fc17d64220715
Panthulumahitha <panthulumahitha0620@gmail.com>
2025-01-28 15:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form element
+    let form = document.getElementById("form1");
 
+    // Retrieve values of first name and last name fields
+    let firstName = form.elements["fname"].value;
+    let lastName = form.elements["lname"].value;
+
+    // Show an alert with the full name
+    alert(firstName + " " + lastName);
 }
